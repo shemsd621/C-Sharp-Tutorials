312c17df776f196856961ac06e19c35b08b1283d
Shemaya Sandanayaka <shemaya.oshi26@gmail.com>
2022-07-01 23:57:43 +0530

    Commit reverse string

---
+++ Part 1 - Basics/ReverseString/ReverseString/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// This program gets a list of words and reverses it
+using System;
+
+namespace ReverseString;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the string that should be reversed : ");
+        string phrase = Console.ReadLine();
+
+        List<string> list = new List<string>();
+        string[] words = phrase.Split(' ');
+        string result = "";
+
+        for (int i = words.Length - 1; i>=0; i--) 
+        {
+            result += words[i] + " ";
+        }
+
+        list.Add(result);
+        foreach(string word in list)
+        {
+            Console.WriteLine("Reversed string : " + word);
+        }
+    }
+}
