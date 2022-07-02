3682789f36622629b8a576037ae3ebf08d8e6bd8
Shemaya Sandanayaka <shemaya.oshi26@gmail.com>
2022-07-02 18:58:55 +0530

    Commit FileSize

---
+++ Part 1 - Basics/FileSize/FileSize/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FileSize
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo("textFile.txt");
+            Console.WriteLine("Size of a file : " + file.Length);
+        }
+    }
+}
