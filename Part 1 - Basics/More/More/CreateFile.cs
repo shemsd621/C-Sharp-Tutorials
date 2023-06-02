778bd3027c0ef0a708ebb5fa1aaa60466a6ece28
Shemaya Sandanayaka <shemaya.oshi26@gmail.com>
2023-06-02 19:30:25 +0530

    Add CreateFile

---
+++ Part 1 - Basics/More/More/CreateFile.cs
@@ -0,0 +1,24 @@
+﻿// Write a program in C# Sharp to create a blank file on the disk.
+
+class CreateFile 
+{
+    public static void Main() 
+    {
+        string fileName = @"myTest.txt";
+
+        try
+        {
+            Console.Write("\nCreate a file named myTest.txt in the disk :\n");
+            Console.Write("----------------------------------------------\n");
+
+            using (FileStream fs = File.Create(fileName)) 
+            {
+                Console.WriteLine("File myTest.txt successfully created!");
+            }
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
